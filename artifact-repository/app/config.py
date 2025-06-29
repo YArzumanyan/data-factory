5f89528df064f97a3bf247eec4510518a60d6a7a
Yervand Arzumanyan <45072978+YArzumanyan@users.noreply.github.com>
2025-06-29 21:20:11 +0200

    Delete artifact-repository

--- artifact-repository/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "artifact_db")
--- artifact-repository/app/core/database.py
+++
@@ -1,5 +0,0 @@
-from app.config import MONGODB_URL, DATABASE_NAME
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient(MONGODB_URL)
-db = client[DATABASE_NAME]
